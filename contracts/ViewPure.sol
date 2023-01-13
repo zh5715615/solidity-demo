35b67595b9a9e8b602f4618b2111ce169f834c84
zh5715615 <zh048388@sina.com>
2023-01-13 21:49:48 +0800

    增加第一个合约

---
+++ contracts/ViewPure.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract ViewPure {
+    string value = "Hello World";
+
+    //pure不能get
+    // function pure_get_func() public pure returns (string memory) {
+    //     return value;
+    // }
+
+    function view_get_func() public view returns (string memory) {
+        return value;
+    }
+
+    //pure不能set
+    // function pure_set_func(string memory _value) public pure {
+    //     value = _value;
+    // }
+
+    //view不能set
+    // function view_set_func(string memory _value) public view {
+    //     value = _value;
+    // }
+
+    function view_set_func(string memory _value) public {
+        value = _value;
+    }
+}
