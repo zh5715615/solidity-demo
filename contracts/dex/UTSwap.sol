244e9094b890957384768bc711e954fd33d327b4
zh5715615 <zh048388@sina.com>
2023-12-06 14:52:04 +0800

    增加新功能

--- contracts/dex/UTSwap.sol
+++ contracts/dex/UTSwap.sol
@@ -26,19 +26,38 @@ contract UTSwap is Context, Ownable {
 
     mapping(address => uint256) userMinings; //矿机用户
 
-    mapping(uint => uint256) miningMachinePrices; //矿机类型对应价格 
+    mapping(address => uint[]) userToolBar; //用户工具栏开通状况
+
+    mapping(address => uint[]) userBackpack; //用户背包栏开通状况
+
+    mapping(uint => uint256) miningMachinePrices; //矿机类型对应价格
+
+    mapping(uint => uint256) mingingPropPrices;   //矿机道具对应价格
+
+    mapping(uint => uint256) mingingFuelPrices;   //矿机燃料对应价格
+
+    mapping(uint => uint256) toolBarPrices;       //开通工具栏对应价格
+
+    mapping(uint => uint256) backpackPrices;      //开通背包栏对应价格
 
     event BuyMiningMachine(address indexed user, uint miningMachineType); //通知运维人员有人买了矿机
 
+    event BuyMiningProp(address indexed user, uint propType); //通知运维人员有人买了道具
+
+    event BuyMiningFuel(address indexed user, uint fuelType); //通知运维人员有人买了燃料
+
+    event OpenToolBar(address indexed user, uint index); //通知运维人员有人付费开通了工具栏
+
+    event OpenBackpack(address indexed user, uint index); //通知运维人员有人付费开通了背包栏
+
     event Transfer(address indexed user, uint256 tatgNumber); //通知运维人员卖出了tatg
 
-    constructor(address beneficiary, address tatgAddress, address usdtAddress, uint _miningMachinePrice) payable Ownable(beneficiary) {
+    constructor(address beneficiary, address tatgAddress, address usdtAddress) payable Ownable(beneficiary) {
         tatgToken = IExpandERC20(tatgAddress);              //初始化tatg代币合约
         usdtToken = IExpandERC20(usdtAddress);              //初始化usdt代币合约
         tatgTokenTotalSupply = tatgToken.totalSupply();     //获取tatgToken的发行量
         usdtTokenDecimals = usdtToken.decimals();           //获取usdt精度
         tatgTokenDecimals = tatgToken.decimals();           //获取tatg精度
-        miningMachinePrices[1] = _miningMachinePrice;       //初太矿机价格
     }
 
     //运维人员使用，添加矿机类型
@@ -47,11 +66,108 @@ contract UTSwap is Context, Ownable {
         miningMachinePrices[miningMachineType] = price;
     }
 
+    //运维人员使用，添加矿机道具类型
+    function addPropType(uint propType, uint256 price) onlyOwner public virtual {
+        //TODO 是否允许多次定义道具价格，如果是则这里就这么写，如果不是则加判重条件
+        mingingPropPrices[propType] = price;
+    }
+
+    //运维人员使用，添加矿机燃料类型
+    function addFuelType(uint fuelType, uint256 price) onlyOwner public virtual {
+        //TODO 是否允许多次定义燃料价格，如果是则这里就这么写，如果不是则加判重条件
+        mingingFuelPrices[fuelType] = price;
+    }
+
+    //运维人员使用，设置工具栏开通价格
+    function setOpenToolBar(uint index, uint256 price) onlyOwner public virtual {
+        //TODO 是否允许多次定义工具栏开通价格，如果是则这里就这么写，如果不是则加判重条件
+        toolBarPrices[index] = price;
+    }
+
+    //运维人员使用，设置背包栏开通价格
+    function setOpenBackpack(uint index, uint256 price) onlyOwner public virtual {
+        //TODO 是否允许多次定义背包栏开通价格，如果是则这里就这么写，如果不是则加判重条件
+        backpackPrices[index] = price;
+    }
+
+    //获取矿机价格
+    function getMingingMachinePrice(uint mingingMachineType) public view virtual returns (uint256) {
+        return miningMachinePrices[mingingMachineType];
+    }
+
     //购买矿机
     function buyMiningMachine(uint miningMachineType) public virtual {
         require(miningMachinePrices[miningMachineType] !=0, "Mining machine type does not exist");
-        SafeERC20.safeTransferFrom(usdtToken, msg.sender, address(this), miningMachinePrices[miningMachineType]);
+        SafeERC20.safeTransferFrom(usdtToken, msg.sender, address(this), getMingingMachinePrice(miningMachineType));
         emit BuyMiningMachine(msg.sender, miningMachineType);
+    }
+
+    //获取道具价格
+    function getMingingPropPrice(uint propType) public view virtual returns (uint256) {
+        return mingingPropPrices[propType];
+    }
+
+    //购买道具
+    function buyProp(uint propType) public virtual {
+        require(mingingFuelPrices[propType] != 0, "Mining prop type does not exist");
+        SafeERC20.safeTransferFrom(tatgToken, msg.sender, address(this), getMingingPropPrice(propType));
+        emit BuyMiningProp(msg.sender, propType);
+    }
+
+    //获取燃料价格
+    function getMingingFuelPrice(uint fuelType) public view virtual returns (uint256) {
+        return mingingFuelPrices[fuelType];
+    }
+
+    //购买燃料
+    function buyFuel(uint fuelType) public virtual {
+        require(mingingFuelPrices[fuelType] != 0, "Mining fuel type does not exist");
+        SafeERC20.safeTransferFrom(tatgToken, msg.sender, address(this), getMingingFuelPrice(fuelType));
+        emit BuyMiningFuel(msg.sender, fuelType);
+    }
+
+    //获取开通工具栏价格
+    function getOpenToolBarPrice(uint index) public view virtual returns (uint256) {
+        return toolBarPrices[index];
+    }
+
+    //获取用户工具栏索引
+    function getUserToolBarIndex(address user) public view virtual returns (uint) {
+        uint userToolBarSize = userToolBar[user].length;
+        uint index = 0;
+        if (userToolBarSize != 0) {
+            index = userToolBarSize;
+        }
+        return index;
+    }
+
+    //开通工具栏
+    function openToolBar() public virtual {
+        uint index = getUserToolBarIndex(msg.sender);
+        SafeERC20.safeTransferFrom(usdtToken, msg.sender, address(this), getOpenToolBarPrice(index));
+        emit OpenToolBar(msg.sender, index);
+    }
+
+    //获取开通背包栏价格
+    function getOpenBackpackPrice(uint index) public view virtual returns (uint256) {
+        return backpackPrices[index];
+    }
+
+    //获取用户工具栏索引
+    function getUserBackpackIndex(address user) public view virtual returns (uint) {
+        uint userBackpackSize = userBackpack[user].length;
+        uint index = 0;
+        if (userBackpackSize != 0) {
+            index = userBackpackSize;
+        }
+        return index;
+    }
+
+    //开通背包栏
+    function openBackpack() public virtual {
+        uint index = getUserBackpackIndex(msg.sender);
+        SafeERC20.safeTransferFrom(usdtToken, msg.sender, address(this), getOpenBackpackPrice(index));
+        emit OpenBackpack(msg.sender, index);
     }
 
     //交易
@@ -70,20 +186,15 @@ contract UTSwap is Context, Ownable {
         userMinings[user] = userMinings[user] + rewardAmount;
     }
 
+    //可提币数量
+    function releasable(address user) public view virtual returns (uint256) {
+        return userMinings[user];
+    }
+
     //提币
     function withdrawal() public virtual {
         require(userMinings[msg.sender] != 0, "The number of withdrawable coins is 0");
-        tatgToken.transfer(msg.sender, userMinings[msg.sender]);
+        tatgToken.transfer(msg.sender, releasable(msg.sender));
         userMinings[msg.sender] = 0;
-    }
-
-    //可提币数量
-    function releasable() public view virtual returns (uint256) {
-        return userMinings[msg.sender];
-    }
-
-    //矿机价格
-    function mingingMachinePrice(uint mingingMachineType) public view virtual returns (uint256) {
-        return miningMachinePrices[mingingMachineType];
     }
 }
