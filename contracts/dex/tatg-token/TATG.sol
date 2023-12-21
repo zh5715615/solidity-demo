b8e2c13beee1f01bdb45604437cd8d4c551e9806
zh5715615 <zh048388@sina.com>
2023-12-21 16:25:26 +0800

    tatg结合swap创建交易对

--- contracts/dex/tatg-token/TATG.sol
+++ contracts/dex/tatg-token/TATG.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "../../ERC20/utils/SafeMath.sol";
+import "../../ERC20/access/Ownable.sol";
 import "../../ERC20/ERC20.sol";
+import "../../swap/IUniswapV2Factory.sol";
+import "../../swap/IUniswapV2Pair.sol";
+import "../../swap/IUniswapV2Router02.sol";
 
 contract TATG is ERC20 {
 
@@ -11,11 +16,13 @@ contract TATG is ERC20 {
 
     uint256 public largeAmount; //锁定期间的限定金额
 
-    constructor(uint64 _lockDuration, uint256 _largeAmount) ERC20("TBTG", "TBTG") {
+    constructor(address initialOwner, uint64 _lockDuration, uint256 _largeAmount) ERC20("TBTG", "TBTG") {
         deployTime = uint64(block.timestamp);
         lockDuration = _lockDuration;
         largeAmount = _largeAmount;
-        _mint(msg.sender, 200000000 * 10 ** decimals()); //发行2亿，精度18
+        _mint(initialOwner, 200000000 * 10 ** decimals()); //发行2亿，精度18
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x3183384179BbA62BEEd7e699916073e633eF37B9);
+        IUniswapV2Factory(_uniswapV2Router.factory()).createPair(address(this), 0x59B6e82Bd9425F69856c9Ff7D715A6273c6959DC);
     }
 
     function releaseTime() public view returns (uint64) {
